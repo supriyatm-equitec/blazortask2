afc76ca79dad591572763288a0d10e6ff2ffdd69
Supriyatm-equitec <supriyatm.equitec@gmail.com>
2024-01-16 10:59:42 +0530

    init

---
+++ BlazorApp1/Data/Empservice.cs
@@ -0,0 +1,19 @@
+﻿using BlazorApp1.Models;
+
+namespace BlazorApp1.Data
+{
+    public class Empservice
+    {
+        private readonly EmpdbContext context;
+
+        public Empservice(EmpdbContext context)
+        {
+            this.context = context;
+        }
+
+        public Task<List<SP_selectResult>> SP_selectAsync()
+        {
+            return context.Procedures.SP_selectAsync();
+        }
+    }
+}
